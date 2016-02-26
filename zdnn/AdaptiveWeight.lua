4056b77ab11ca61df1a65a232d9dd9bf6deb9754
Wen Li Zhuang <wlzhuang@nlg.csie.ntu.edu.tw>
2016-02-26 18:20:27 +0000

    add AdaptiveWeight.lua

---
+++ zdnn/AdaptiveWeight.lua
@@ -0,0 +1,20 @@
+
+local Class,Parent = torch.class('zdnn.AdaptiveWeight', 'nn.Sequential')
+
+function Class:__init( dim, bias )
+	Parent.__init(self)
+
+	self.dim = dim
+
+	local module = nn.Linear(dim,1,bias)
+	self :add( module )              -- N x 1
+		 :add(nn.View(-1))           -- N
+		 :add(nn.SoftMax())          -- N
+		 :add(nn.View(-1,1))         -- N x 1
+end
+
+function Class:__tostring()
+	return torch.type(self) .. string.format( "( [n,%d] -> [n,1] )",
+		self.dim
+	)
+end
